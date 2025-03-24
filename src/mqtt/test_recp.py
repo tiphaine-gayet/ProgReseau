28275806796c5aaf9013059c702fa310a4b5de9e
tiphaine-gayet <tiphaine.gayet@gmail.com>
2025-03-24 11:10:55 +0100

    refator message

--- src/mqtt/test_recp.py
+++ src/mqtt/test_recp.py
@@ -45,37 +45,38 @@ def set_receive_infra(client):
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        message = msg.payload.decode("utf-8")
         if msg.topic == "python/ctrlrobot":
-            if "start" in msg.payload.decode("utf-8"):
+            if "start" in message:
                 move.start()
-            if "left" in msg.payload.decode("utf-8"):
+            if "left" in message:
                 move.left()
-            if "right" in msg.payload.decode("utf-8"):
+            if "right" in message:
                 move.right()
-            if "back" in msg.payload.decode("utf-8"):
+            if "back" in message:
                 move.back()
-            if "stop" in msg.payload.decode("utf-8"):
+            if "stop" in message:
                 move.stop()
-            if "tir" in msg.payload.decode():
+            if "tir" in message:
                 infra.shoot()
                 led.blink(r=255, g=0, b=0, time_sec=0.2)
         if msg.topic == "tanks/id/init":
-            if "TEAM BLUE" in msg.payload.decode():
+            if "TEAM BLUE" in message:
                 led.blink(r=0, g=0, b=255, time_sec=1)
-            if "TEAM RED" in msg.payload.decode():
+            if "TEAM RED" in message:
                 led.blink(r=255, g=0, b=0, time_sec=1)
         if msg.topic == "tanks/id/shots/in":
-            if "SHOT" in msg.payload.decode():
+            if "SHOT" in message:
                 led.blink_shot()
-            if "FLAG_LOST" in  msg.payload.decode():
+            if "FLAG_LOST" in  message:
                 led.blink(r=255, g=0, b=0, time_sec=1)
-            if "ABORT_CATCHING_SHOT" in msg.payload.decode():
+            if "ABORT_CATCHING_SHOT" in message:
                 led.blink(r=255, g=0, b=0, time_sec=1)
                 pass
         if msg.topic == "tanks/id/shots/out":
-            if "FRIENDLY_FIRE" in msg.payload.decode():
+            if "FRIENDLY_FIRE" in message:
                 led.blink(0.5)
-            if "SHOT" in msg.payload.decode():
+            if "SHOT" in message:
                 led.blink(r=0,g=255,b=0, time_sec=1)
 
 
