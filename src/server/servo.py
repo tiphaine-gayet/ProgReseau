249e96fe944ed68d83a6c0e633adb2b790e46ec6
KoviazinaA <aleksandra.koviazina03@gmail.com>
2025-03-31 14:23:35 +0200

    we tried

--- src/server/servo.py
+++ src/server/servo.py
@@ -5,12 +5,12 @@ pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
 
 # Essayer une position centrale
-pwm.set_pwm(0, 0, 300)
+pwm.set_pwm(2, 0, 300)
 time.sleep(2)
 
 # Essayer une position extrême
-pwm.set_pwm(0, 0, 150)
+pwm.set_pwm(2, 0, 150)
 time.sleep(2)
 
 # Retour à la position centrale
-pwm.set_pwm(0, 0, 300)
+pwm.set_pwm(2, 0, 300)
